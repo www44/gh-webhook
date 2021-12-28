b41fa61092e4efe8bae5ddaabacef82d18a13940
www44 <6235439+www44@users.noreply.github.com>
2021-12-28 15:36:08 +0100

    non functional syntax improvement

--- webhook/service.py
+++ webhook/service.py
@@ -1,24 +1,41 @@
-from github import Github
-from github import GithubException
-import json
-import sys
+"""
+Python implementation of GitHub webhook for protecting main branch
+"""
+
 import os
-import boto3
 import base64
-from botocore.exceptions import ClientError
+import boto3
+from github import Github
 
 
 def handler(event, context):
-    """webhook handler that make default branch protected 
-    and creates GitHub issue
+    """
+    webhook handler that make default branch protected
+    and creates GitHub issue.
+
+    Parameters
+    -----------
+    event: dict
+        GitHub webhook payload
+    context: dict
+        This object provides methods and properties that provide
+        information about the invocation, function, and execution
+        environment.
+
+    Returns
+    -------
+    output: dict
+        Information about execution result.
+        Pong - for ping command
+        Branch status update summary or error messages.
     """
 
-    # define output variable basic structure
-    output = dict()
+    # define output variable structure
+    output = {}
     output["Content-Type"] = "application/json"
     output["statusCode"] = 200
 
-    #answer for ping event
+    # answer for ping event
     if event.get('hook'):
         output["body"] = {"message": 'pong'}
         return output
@@ -31,18 +48,29 @@ def handler(event, context):
     repo = github.get_repo(event['repository']['full_name'])
     branch = repo.get_branch(os.environ['git_default_branch'])
     branch.edit_protection(True)
-    
+
     # create issue, mentioning author in description
     username = event['sender']['login']
     repo.create_issue(title="protect branch",
-                      body='@{}, main branch is protected now'.format(username))
-    output["body"] = {"message": 'branch {} is protected now'.format(branch.name)}
+                      body=f"@{username}, main branch is protected now")
+    output["body"] = {"message": f"branch {branch.name} is protected now"}
     return output
 
 
 def get_secret(secret_name, region_name) -> str:
-    """ return secret as plain text out of
-    AWS Secret Manager
+    """
+    Get secret out of AWS Secret Manager
+
+    Parameters
+    ----------
+    secret_name: str
+        Name of AWS secret to obtain
+    region_name: str
+        Name of AWS region, where secret is stored
+
+    Returns
+    -------
+    secret as plain text: str
     """
     session = boto3.session.Session()
     client = session.client(
@@ -53,5 +81,4 @@ def get_secret(secret_name, region_name) -> str:
 
     if 'SecretString' in get_secret_value_response:
         return get_secret_value_response['SecretString']
-    else:
-        return base64.b64decode(get_secret_value_response['SecretBinary'])
+    return base64.b64decode(get_secret_value_response['SecretBinary'])
